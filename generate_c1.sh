ab87e1a3a1cc0f499965e3f5ddee3910faf13205
Md Rahman <md.rahman@intel.com>
2024-09-04 10:33:33 -0700

    Updated scripts

--- generate_c1.sh
+++ generate_c1.sh
@@ -1,28 +1,35 @@
 #!/bin/sh
 
-export SHMEM_DISABLE_MULTIRAIL=1
+# Env variables
+export SHMEM_OFI_DISABLE_MULTIRAIL=1
 export FI_CXI_DEFAULT_CQ_SIZE=131072
 export FI_PROVIDER=cxi
 export FI_CXI_OPTIMIZED_MRS=0
 export EnableImplicitScaling=0
-mpiexec -n 1 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/put_bw -t device -m 4194304 > tmp_1
+
+# Put 
+mpiexec -n 1 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/put_bw -t device -m 4194304 | grep 'test put_bw' > tmp_1
 cat tmp_1 | awk {'printf("%ld\t%lf\n", $19, $26)'} > tmp_same
 
-mpiexec -n 2 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/put_bw -t device -m 4194304 > tmp_1
+mpiexec -n 2 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/put_bw -t device -m 4194304 | grep 'test put_bw' > tmp_1
 cat tmp_1 | awk {'printf("%lf\n", $26)'} > tmp_tile
 
-mpiexec -n 3 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/put_bw -t device -m 4194304 > tmp_1
+mpiexec -n 3 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/put_bw -t device -m 4194304 | grep 'test put_bw' > tmp_1
 cat tmp_1 | awk {'printf("%lf\n", $26)'} > tmp_device
 
 paste tmp_same tmp_tile tmp_device > ishmem_put
 
-mpiexec -n 1 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/get_bw -t device -m 4194304 > tmp_1
+# Get
+mpiexec -n 1 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/get_bw -t device -m 4194304 | grep 'test get_bw' > tmp_1
 cat tmp_1 | awk {'printf("%ld\t%lf\n", $19, $26)'} > tmp_same
 
-mpiexec -n 2 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/get_bw -t device -m 4194304 > tmp_1
+mpiexec -n 2 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/get_bw -t device -m 4194304 | grep 'test get_bw' > tmp_1
 cat tmp_1 | awk {'printf("%lf\n", $26)'} > tmp_tile
 
-mpiexec -n 3 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/get_bw -t device -m 4194304 > tmp_1
+mpiexec -n 3 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/get_bw -t device -m 4194304 | grep 'test get_bw' > tmp_1
 cat tmp_1 | awk {'printf("%lf\n", $26)'} > tmp_device
 
 paste tmp_same tmp_tile tmp_device > ishmem_get
+
+# Cleanup
+rm ./tmp*
---
+++ generate_c2_2.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+export SHMEM_OFI_DISABLE_MULTIRAIL=1
+export FI_CXI_DEFAULT_CQ_SIZE=131072
+export FI_PROVIDER=cxi
+export FI_CXI_OPTIMIZED_MRS=0
+export EnableImplicitScaling=0
+
+# Put with cutover always
+# Please build first with the appropriate patch as shown below
+## cd $ISHMEM_BUILD_DIR/..
+## git apply ishmem_cutover_always.patch
+## cd $ISHMEM_BUILD_DIR
+## make -j
+#
+
+mpiexec -n 3 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/put_bw -m 4194304 | grep 'test put_bw' | grep 'group<1>' | grep 'threads 1 ' > tmp_1
+cat tmp_1 | awk {'printf("%ld\t%lf\n", $20, $27)'} > tmp_t1
+
+mpiexec -n 3 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/put_bw -m 4194304 | grep 'test put_bw' | grep 'group<1>' | grep 'threads 16 ' > tmp_1
+cat tmp_1 | awk {'printf("%lf\n", $27)'} > tmp_t16
+
+mpiexec -n 3 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/put_bw -m 4194304 | grep 'test put_bw' | grep 'group<1>' | grep 'threads 128 ' > tmp_1
+cat tmp_1 | awk {'printf("%lf\n", $27)'} > tmp_t128
+
+mpiexec -n 3 $ISHMEM_BUILD_DIR/../scripts/ishmrun $ISHMEM_BUILD_DIR/test/performance/put_bw -m 4194304 | grep 'test put_bw' | grep 'group<1>' | grep 'threads 1024 ' > tmp_1
+cat tmp_1 | awk {'printf("%lf\n", $27)'} > tmp_t1024
+
+paste tmp_t1 tmp_t16 tmp_t128 tmp_t1024 > ishmem_cutover_always
+
